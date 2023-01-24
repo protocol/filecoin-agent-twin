337f1297774b4649072880a4a2f1b7a361d86dd8
Tom Mellan <t.mellan@imperial.ac.uk>
2023-01-24 12:11:24 +0000

    to allow setup --- typo in version, adding mesa package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="agentFIL",
-    verison="0.1",
+    version="0.1",
     author="Kiran Karra, Tom Mellan",
     author_email="kiran.karra@gmail.com, t.mellan@imperial.ac.uk",
     description="Agent based model for Filecoin Economy",
@@ -16,7 +16,7 @@ setuptools.setup(
         "Source": "https://github.com/protocol/filecoin-agent-twin",
     },
     packages=setuptools.find_packages(),
-    install_requires=["numpy>=1.23.1", "pandas>=1.4.3", "requests>=2.28.1"],
+    install_requires=["numpy>=1.23.1", "pandas>=1.4.3", "requests>=2.28.1", "mesa", "scipy"],
     python_requires=">=3.8",
     classifiers=[
         "Programming Language :: Python :: 3",
